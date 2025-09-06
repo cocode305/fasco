e7dcb04424749da5a35f982c963a8bfa267a3c0a
CoCo <eneluwecc@gmail.com>
2025-09-06 06:50:51 +0100

    Had to remove the document.querySelectorAll() from the mobile nav menu since I'll only have one menu par page. Also added an overlay effect when the menu opens

--- app.js
+++ app.js
@@ -1,19 +1,28 @@
 //MOBILE NAV
-const openMnav = document.querySelectorAll(".mnav_open-btn");
+const openMnav = document.querySelector(".mnav_open-btn");
 const mnav = document.querySelector(".mnav");
-const closeMnav = document.querySelectorAll(".mnav_close-btn");
+const closeMnav = document.querySelector(".mnav_close-btn");
+const overlay = document.getElementById("overlay");
 
-openMnav.forEach((btn) => {
-    btn.addEventListener("click", () => {
+openMnav.addEventListener("click", () => {
         mnav.classList.remove("translate-x-full", "opacity-0");
         mnav.classList.add("translate-x-0", "opacity-100");
-    });
+        overlay.classList.remove("opacity-0", "pointer-events-none");
+        overlay.classList.add("opacity-100");
 });
-closeMnav.forEach((btn) => {
-    btn.addEventListener("click", () => {
+closeMnav.addEventListener("click", () => {
         mnav.classList.remove("translate-x-0", "opacity-100");
         mnav.classList.add("translate-x-full", "opacity-0");
+        overlay.classList.add("opacity-0", "pointer-events-none");
+        overlay.classList.remove("opacity-100");
     });
+
+// close if overlay clicked
+overlay.addEventListener("click", () => {
+    mnav.classList.remove("translate-x-0", "opacity-100");
+    mnav.classList.add("translate-x-full", "opacity-0");
+    overlay.classList.add("opacity-0", "pointer-events-none");
+    overlay.classList.remove("opacity-100");
 });
 
 // DROPDOWN
